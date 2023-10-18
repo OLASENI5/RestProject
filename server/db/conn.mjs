e023f9e1c89b16642926f26a7efb35a2b802252f
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2023-10-18 09:30:57 +0100

    commit

---
+++ server/db/conn.mjs
@@ -0,0 +1,20 @@
+
+import { MongoClient } from "mongodb"
+
+const connectionString = process.env.ATLAS_URI;
+const client = new MongoClient(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+let dbConnection;
+
+module.exports = {
+  connectToServer: function (callback) {
+    // Implement Database connection
+  },
+
+  getDb: function () {
+    return dbConnection;
+  },
+};
---
+++ server/index.mjs
@@ -0,0 +1,9 @@
+
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json())
